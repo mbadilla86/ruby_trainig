fd237511a821ec2bbafe776968f6878a89ccfd70
Mauricio Badilla <mbadilla86@gmail.com>
2021-01-26 17:05:27 -0600

    ruby basico

---
+++ strings.rb
@@ -0,0 +1,8 @@
+a = "esto es un lindo string"
+b = "otro string"
+# en ruby casi todo es un objeto
+puts a.length
+puts b.length
+# string de multiples lineas
+c = "esto es\nstring\nmultiples lineas"
+puts c
---
+++ variables.rb
@@ -0,0 +1,12 @@
+# Ruby puede tener números enteros
+puts " Enteros "
+a = 37
+a + 1
+puts a
+a = a + 1
+puts a 
+# ruby tambien puede tener decimales
+b = 2.5
+c = 1
+puts"decimales"
+puts b + c # El resultado de un entero mas un decimal es decimal
