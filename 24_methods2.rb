f5bb94b4941246755d356c43c632e8c9b7e79ff7
Mauricio Badilla <mbadilla86@gmail.com>
2021-01-29 14:47:12 -0600

    arrays and methods

---
+++ 24_methods2.rb
@@ -0,0 +1,7 @@
+def say_hello(name,last_name = "rojas") 
+    horoscope = "capricornio"
+    "hola #{name.capitalize} #{last_name.capitalize}, pura vida #{horoscope}"
+end
+
+puts say_hello("justin","bieber")
+puts say_hello("justin")
